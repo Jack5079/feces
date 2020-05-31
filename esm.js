e9eb228f23f5e25b8532a25d1a4f5ac64b8e0845
Jack <29169102+Jack5079@users.noreply.github.com>
2020-05-30 21:49:26 -0400

    Add cors support

--- esm.js
+++ esm.js
@@ -7,7 +7,12 @@ import fetch from 'https://cdn.pika.dev/isomorphic-fetch'
  * @returns {Promise<string>} A URL of a piece of shit.
  */
 async function shit () {
-  const [{ data: { children: [{ data: { url } }] } }] = await fetch('https://www.reddit.com/r/poop/random.json').then(res => res.json())
-  return url
+  try {
+    const [{ data: { children: [{ data: { url } }] } }] = await fetch('https://www.reddit.com/r/poop/random.json').then(res => res.json())
+    return url
+  } catch {
+    const [{ data: { children: [{ data: { url } }] } }] = await fetch(`https://api.allorigins.win/get?url=${encodeURIComponent('https://www.reddit.com/r/poop/random.json')}`).then(res => res.json())
+    return url
+  }
 }
 export default shit
