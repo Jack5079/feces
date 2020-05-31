3019a1a680187c0049ebbde764993d7336f8a962
Jack <29169102+Jack5079@users.noreply.github.com>
2020-05-31 16:31:35 -0400

    add to esm cli and test

--- esm.mjs
+++ esm.mjs
@@ -10,6 +10,11 @@ export default () => fetch('https://api.allorigins.win/get?url=https://www.reddi
   .then(res => res.json())
   .then(({ contents }) => JSON.parse(contents))
   .then(([{ data: { children: [{ data: { url } }] } }]) => url)
+/**
+* Get a massive piece of shit from r/Phonkers.
+*
+* @returns {Promise<string>} A URL of a piece of shit.
+*/
 export const massive = () => fetch('https://api.allorigins.win/get?url=https://www.reddit.com/r/Phonkers/random.json')
   .then(res => res.json())
   .then(({ contents }) => JSON.parse(contents))
--- test.mjs
+++ test.mjs
@@ -1,9 +1,10 @@
-import feces from './esm.mjs'
+import feces, {massive as phonk} from './esm.mjs'
 
 (async () => {
 
   const assert = console.assert
   
   assert((await feces()).startsWith('https://i.redd.it/'))
+  assert((await phonk()).startsWith('https://i.redd.it/'))
 
 })()
